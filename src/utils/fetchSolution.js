24549719f04a0fc73e490ac19a55865420dde463
Serhii Lavreniuk <lavreniuksehii02@gmail.com>
2024-11-17 23:51:50 +0100

    changed fetchSolution

--- src/utils/fetchSolution.js
+++ src/utils/fetchSolution.js
@@ -14,25 +14,20 @@ const fetchData = async (url, setData, setError) => {
 
 export const fetchSolution = async (cols, rows, posX, posY, selectedMethods, setSolutionW, setSolutionGA, setError) => {
     const { warnsdorf, genetic } = selectedMethods;
-    const baseUrl = `http://localhost:8080/solutions?col=${cols}&row=${rows}&posKnightX=${posX - 1}&posKnightY=${posY - 1}`;
+
+    const urlW = `http://localhost:8080/solutions/warnsdorf?col=${cols}&row=${rows}&posKnightX=${posX - 1}&posKnightY=${posY - 1}`;
+    const urlG = `http://localhost:8080/solutions/genetic-approach?col=${cols}&row=${rows}&posKnightX=${posX - 1}&posKnightY=${posY - 1}`;
+
 
     if (warnsdorf && !genetic) {
-        const url = `${baseUrl}&method=warnsdorf`;
-        fetchData(url, setSolutionW, setError);
+        fetchData(urlW, setSolutionW, setError);
     } else if (!warnsdorf && genetic) {
-        const url = `${baseUrl}&method=genetic-approach`;
-        fetchData(url, setSolutionGA, setError);
+        fetchData(urlG, setSolutionGA, setError);
     } else if (warnsdorf && genetic) {
-        const urlWarnsdorf = `${baseUrl}&method=warnsdorf`;
-        const urlGenetic = `${baseUrl}&method=genetic-approach`;
+        fetchData(urlW,setSolutionW,setError);
+        fetchData(urlG,setSolutionGA,setError);
 
-        // Выполнение обоих запросов параллельно
-        Promise.all([
-            fetchData(urlWarnsdorf, setSolutionW, setError),
-            fetchData(urlGenetic, setSolutionGA, setError)
-        ]).catch((error) => {
-            setError(error.message);
-        });
+        console.log("W && GA");
     } else {
         setError("The solution method is not chosen");
     }
