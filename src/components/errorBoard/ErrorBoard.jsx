6b33cb645184e3aabd9335f675db18bdaf80810e
Serhii Lavreniuk <lavreniuksehii02@gmail.com>
2025-01-28 10:36:07 +0100

    go

--- src/components/errorBoard/ErrorBoard.jsx
+++ src/components/errorBoard/ErrorBoard.jsx
@@ -6,12 +6,13 @@ import Error1 from './Error';
 const ErrorBoard = ({ messages }) => {
     return (
         <div>
-            <div
-                className="error-title boots"
-                style={{ fontSize: "15px" }}
-            >
-                ERROR
-            </div>
+            {Array.isArray(messages) && messages.length > 0 ? (
+                <div className="error-title boots" style={{ fontSize: "15px" }}>
+                    ERROR
+                </div>
+            ):(<div className="error-title boots" style={{ fontSize: "15px" }}>
+            </div>)
+            }
             {Array.isArray(messages) && messages.length > 0 ? (
                 messages.map((message, index) => (
                     <Error1 key={index} message={message} />
